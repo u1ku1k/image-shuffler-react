2c9d3db06c57f302ac31104656b9ce99817c61fc
Голованев Виталий Евгеньевич <vitalii.golovanev@fsight.ru>
2018-06-05 18:10:48 +0500

    Webpack first configuration

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import css from './style.css';
 import bootstrap from 'bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
-  render() { }
+  render() {
+    return <h1>test123</h1>
+  }
 }
 
-ReactDOM.render(App, null, document.getElementById('react-container'));
+ReactDOM.render(<App/>, document.getElementById('react-container'));
