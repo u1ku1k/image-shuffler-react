0784573dd6a14b29010fd928b37caaf285a93af0
Vitalii Golovanev <v.golovanev@gmail.com>
2018-06-05 08:08:49 +0500

    Preparing to implement layout

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import css from './style.css';
+import bootstrap from 'bootstrap';
+
+class App extends React.Component {
+  render() { }
+}
+
+ReactDOM.render(App, null, document.getElementById('react-container'));
